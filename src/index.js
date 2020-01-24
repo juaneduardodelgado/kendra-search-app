a0a31193e7fb0f86190aa2d1d03f920b06d1ab81
Aziz <syedazi@38f9d3716238.ant.amazon.com>
2020-01-24 17:29:45 +0000

    Added routing to the app

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
+
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
@@ -8,7 +10,17 @@ import config from './aws-exports'
 import Amplify, { Auth } from 'aws-amplify';
 Amplify.configure(config);
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const routing = (
+		<Router>
+			<Switch>
+				<Route exact path ="/" component={Login}/>
+				<Route exact path ="/signup" component={Signup}/>
+				<Route exact path ="/chat" component={Chat}/>
+				<Route component={NotFound} />
+			</Switch>
+		</Router>
+	)
+ReactDOM.render(routing, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
