a546deb45f634e2e6d631c2fe9399c4bc7b2ee87
Aziz <syedazi@38f9d3716238.ant.amazon.com>
2020-01-24 13:13:03 +0000

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,135 @@
-import React from 'react';
+import React, { Component } from 'react';
+
+import Amplify, { Interactions } from 'aws-amplify';
+
+import API, { graphqlOperation } from '@aws-amplify/api';
+import PubSub from '@aws-amplify/pubsub';
+
+import { createTodo } from './graphql/mutations';
+
+import { ChatFeed, Message } from 'react-chat-ui'
+
+import awsconfig from './aws-exports';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+API.configure(awsconfig);
+PubSub.configure(awsconfig);
+
+async function createNewTodo() {
+  const todo = { name: "Sample Todo object", description: "Realtime and offline"};
+  await API.graphql(graphqlOperation(createTodo, { input: todo }));
 }
 
-export default App;
+class App extends Component {
+  state = {
+    input: '',
+    finalMessage: '',
+    messages: [
+      new Message({
+        id: 1,
+        message: "Hello, how can I help you today?",
+      })
+    ]
+  }
+  _handleKeyPress = (e) => {
+    if (e.key === 'Enter') {
+      this.submitMessage()
+    }
+  }
+  onChange(e) {
+    const input = e.target.value
+    this.setState({
+      input
+    })
+  }
+  async submitMessage() {
+    const { input } = this.state
+    if (input === '') return
+    const message = new Message({
+      id: 0,
+      message: input,
+    })
+    let messages = [...this.state.messages, message]
+
+    this.setState({
+      messages,
+      input: ''
+    })
+    const response = await Interactions.send("KendraBot", input)
+    const responseMessage = new Message({
+      id: 1,
+      message: response.message
+    })
+    messages = [...this.state.messages, responseMessage]
+    this.setState({ messages })
+
+    if (response.dialogState === 'Fulfilled') {
+      if (response.intentName === 'SearchIntent') {
+        const finalMessage = response.message
+        this.setState({ finalMessage })
+      }
+    }
+  }
+  render() {
+    return (
+      <div className="App">
+        <header style={styles.header}>
+          <p style={styles.headerTitle}>Welcome to Kendra Search Bot!</p>
+        </header>
+        <div style={styles.messagesContainer}>
+        <h2>Welcome to Kendra Search</h2>
+        <ChatFeed
+          messages={this.state.messages}
+          hasInputField={false}
+          bubbleStyles={styles.bubbleStyles}
+        />
+
+        <input
+          onKeyPress={this._handleKeyPress}
+          onChange={this.onChange.bind(this)}
+          style={styles.input}
+          value={this.state.input}
+        />
+        </div>
+      </div>
+    );
+  }
+}
+
+const styles = {
+  bubbleStyles: {
+    text: {
+      fontSize: 16,
+    },
+    chatbubble: {
+      borderRadius: 30,
+      padding: 10
+    }
+  },
+  headerTitle: {
+    color: 'white',
+    fontSize: 22
+  },
+  header: {
+    backgroundColor: 'rgb(0, 132, 255)',
+    padding: 20,
+    borderTop: '12px solid rgb(204, 204, 204)'
+  },
+  messagesContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+    padding: 10,
+    alignItems: 'center'
+  },
+  input: {
+    fontSize: 16,
+    padding: 10,
+    outline: 'none',
+    width: 350,
+    border: 'none',
+    borderBottom: '2px solid rgb(0, 132, 255)'
+  }
+}
+
+export default App
